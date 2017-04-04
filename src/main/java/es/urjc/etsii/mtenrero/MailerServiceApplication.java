118e6ff8e87534b660984e403c79ada4b6d3af77
Adrián <adrxxm@gmail.com>
2017-04-04 17:03:09 +0200

    Initial Commit

---
+++ src/main/java/es/urjc/etsii/mtenrero/MailerServiceApplication.java
@@ -0,0 +1,12 @@
+package es.urjc.etsii.mtenrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailerServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/es/urjc/etsii/mtenrero/MailerServiceController.java
@@ -0,0 +1,67 @@
+package es.urjc.etsii.mtenrero;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.MailSender;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+
+/**
+ * Created by Adrian on 4/4/17.
+ */
+
+@RestController
+public class MailerServiceController {
+
+    final String fromEmail = "vetmanagerApp@gmail.com";
+    final MailerResponse emailSentSuccesfully = new MailerResponse(true,"Your email was succesfully sent!");
+    final MailerResponse emailNotValid = new MailerResponse(false,"The email address you provided isn't valid");
+
+
+    @Autowired
+    private MailSender mail;
+
+    @RequestMapping(value = "/sendEmail", method = RequestMethod.POST)
+    public ResponseEntity<MailerResponse> sendEmail(@RequestParam("email") String email,@RequestParam("subject") String subject, @RequestParam("body") String body) {
+        if (!validAddress(email)) {
+            return new ResponseEntity<>(this.emailNotValid, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            this.buildEmailAndSend(email,this.fromEmail,subject,body);
+            return new ResponseEntity<>(this.emailSentSuccesfully, HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            MailerResponse errorResponse = new MailerResponse(false,e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public static boolean validAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+    public synchronized void buildEmailAndSend(String toAddress, String fromAddress, String subject, String msgBody) {
+        SimpleMailMessage Msg = new SimpleMailMessage();
+        Msg.setFrom(fromAddress);
+        Msg.setTo(toAddress);
+        Msg.setSubject(subject);
+        Msg.setText(msgBody);
+        mail.send();
+    }
+
+
+}
